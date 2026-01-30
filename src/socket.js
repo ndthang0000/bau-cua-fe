9223e0091eae94690ab52ce9ed70e27713596b3b
ndthang0000 <ndthang0000@gmail.com>
2026-01-30 22:24:13 +0700

    fix: fix socket URL

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 // Thay đổi URL này thành IP máy tính của bạn khi test trên điện thoại
-const SOCKET_URL = "192.168.1.177:3125"//"https://baucua-sever.thandoso.com"//"192.168.1.16:3125";
+const SOCKET_URL = "https://baucua-sever.thandoso.com"//"https://baucua-sever.thandoso.com"//"192.168.1.16:3125";
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false, // Chúng ta sẽ chủ động connect khi user vào phòng
